d35ac586328417c79579533a8359e9f9bebe0f4b
OshiriChan <oshirichan252@gmail.com>
2016-11-01 21:39:30 +0900

    second

--- js/background.js
+++ js/background.js
@@ -1,8 +1,15 @@
 $(function(){
   var stream_community = new Array();
   var stream_name = new Array();
+  var setTimer;
+  var timerflag = false;
   if (localStorage.streamflag == undefined) {
     localStorage.streamflag = "0";
+  } else {
+    if (localStorage.streamflag == "1") {
+      timerflag = true;
+      startTimer();
+    }
   }
   /*
   * popup.jsからのメッセージ受信
@@ -14,10 +21,20 @@ $(function(){
     function(request, sender, sendResponse) {
       //console.log(request.comget);
       if (request.comget != undefined) {
+        console.log(stream_community);
+        console.log(stream_name);
         sendResponse({com:stream_community, name:stream_name});
       } else if (request.streamflag != undefined) {
-        console.log(request.streamflag);
-        localStorage.streamflag= request.streamflag;
+        if (request.streamflag == "1") {
+          timerflag = true;
+          startTimer();
+        } else if (request.streamflag == "0" && timerflag == true) {
+          stream_community = new Array();
+          stream_name = new Array();
+          timerflag = false;
+          stopTimer();
+        }
+        localStorage.streamflag = request.streamflag;
       }
       if ((localStorage.mail == undefined || localStorage.password == undefined) && request.greeting == "first") {
         sendResponse({res: "non"});
@@ -55,7 +72,9 @@ $(function(){
               success: function (res) {
                 for (var i = 0; i < $(res).find("community_id").length; i++) {
                   //console.log($(res).find("community_id")[i].childNodes[0].nodeValue);
-                  community_num.push($(res).find("community_id")[i].childNodes[0].nodeValue);
+                  if ($(res).find("community_id")[i].childNodes[0].nodeValue.indexOf("co") != -1) {
+                    community_num.push($(res).find("community_id")[i].childNodes[0].nodeValue);
+                  }
                 }
                 //console.log($(res).find("community_id").length);
                 localStorage.setItem("community", community_num);
@@ -69,36 +88,90 @@ $(function(){
     }
   );
 
-  if (localStorage.getItem("community") != undefined) {
-    setInterval(function(){
-      if (localStorage.streamflag == "1") {
+//   if (localStorage.getItem("community") != undefined) {
+//     //setInterval(function(){
+//       if (localStorage.streamflag == "1") {
+//         var communities = localStorage.getItem("community").split(",");
+//         stream_community = new Array();
+//         stream_name = new Array();
+//         var cnt = 0;
+//         var i = 0;
+//         var timeinterval = setInterval(function(){
+//           console.log(i);
+//           if (i == communities.length) {
+//             console.log("ok");
+//             clearInterval(timeinterval);
+//           }
+//           $.ajax({
+//             url: "http://live.nicovideo.jp/api/getplayerstatus?v="+communities[i],
+//             type: "GET",
+//             dataType: "xml",
+//             cache : false,
+//             success: function (res) {
+//               if ($(res)[0].childNodes[0].attributes[0].nodeValue == "fail") {
+//                 return;
+//               }
+//               var live_status = $(res).find("archive")[0].childNodes[0].nodeValue; // 0:stream, 1:timeshift
+//               if (live_status == "0") {
+//                 //console.log($(res).find("default_community")[0].childNodes[0].nodeValue);
+//                 stream_community.push($(res).find("default_community")[0].childNodes[0].nodeValue);
+//                 stream_name.push($(res).find("owner_name")[0].childNodes[0].nodeValue);
+//                 cnt++;
+//                 chrome.browserAction.setBadgeText({text:String(cnt)});
+//               }
+//             }
+//           });
+//           i++;
+//         }, 500);
+//       }
+//     //},1000);
+//   }
+// //  },5000);
+
+  function startTimer () {
+    var i = 0;
+    var cnt = 0;
+    var save_com = new Array();
+    var save_com_name = new Array();
+    setTimer = setInterval (function () {
+      if (localStorage.getItem("community") != undefined) {
         var communities = localStorage.getItem("community").split(",");
-        stream_community = new Array();
-        stream_name = new Array();
-        var cnt = 0;
-        for (var i = 0; i < communities.length; i++) {
-          $.ajax({
-            url: "http://live.nicovideo.jp/api/getplayerstatus?v="+communities[i],
-            type: "GET",
-            dataType: "xml",
-            cache : false,
-            success: function (res) {
-              if ($(res)[0].childNodes[0].attributes[0].nodeValue == "fail") {
-                return;
-              }
-              var live_status = $(res).find("archive")[0].childNodes[0].nodeValue; // 0:stream, 1:timeshift
-              if (live_status == "0") {
-                //console.log($(res).find("default_community")[0].childNodes[0].nodeValue);
-                stream_community.push($(res).find("default_community")[0].childNodes[0].nodeValue);
-                stream_name.push($(res).find("owner_name")[0].childNodes[0].nodeValue);
-                cnt++;
-                chrome.browserAction.setBadgeText({text:String(cnt)});
-              }
-            }
-          });
+        if (i == communities.length) {
+          i = 0;
+          chrome.browserAction.setBadgeText({text:String(cnt)});
+          stream_community = save_com;
+          stream_name = save_com_name;
+          save_com = new Array();
+          save_com_name = new Array();
+          cnt = 0;
         }
+        $.ajax({
+          url: "http://live.nicovideo.jp/api/getplayerstatus?v="+communities[i],
+          type: "GET",
+          dataType: "xml",
+          cache : false,
+          success: function (res) {
+            if ($(res)[0].childNodes[0].attributes[0].nodeValue == "fail") {
+              return;
+            }
+            var live_status = $(res).find("archive")[0].childNodes[0].nodeValue; // 0:stream, 1:timeshift
+            if (live_status == "0") {
+              //console.log($(res).find("default_community")[0].childNodes[0].nodeValue);
+              console.log($(res).find("owner_name")[0].childNodes[0].nodeValue);
+              save_com.push($(res).find("default_community")[0].childNodes[0].nodeValue);
+              save_com_name.push($(res).find("owner_name")[0].childNodes[0].nodeValue);
+              cnt++;
+            }
+          }
+        });
+        i++;
+      } else {
+        console.log("not save community");
       }
-    },10000);
+    }, 1000);
   }
-//  },5000);
+
+  function stopTimer () {
+    clearInterval(setTimer);
+  }
 });
